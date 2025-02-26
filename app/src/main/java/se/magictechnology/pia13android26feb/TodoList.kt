69047d9dad6582c2592d6669cb35094882a91d65
Bill Mårtensson <bill@magictechnology.se>
2025-02-26 11:30:23 +0100

    pia13

--- app/src/main/java/se/magictechnology/pia13android26feb/TodoList.kt
+++ app/src/main/java/se/magictechnology/pia13android26feb/TodoList.kt
@@ -1,19 +1,71 @@
 package se.magictechnology.pia13android26feb
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun TodoList() {
+fun TodoList(todoviewmodel : TodoViewModel = viewModel()) {
 
+    var addtodotitle by remember { mutableStateOf("") }
 
+    val alltodo by todoviewmodel.alltodo.collectAsState()
+
+    LaunchedEffect(true) {
+        todoviewmodel.loadTodo()
+    }
 
     Column(modifier = Modifier.fillMaxSize()) {
         Text("TODO")
+
+
+        Row {
+            TextField(value = addtodotitle, onValueChange = {addtodotitle = it})
+
+            Button(onClick = {
+                todoviewmodel.addTodo(addtodotitle)
+            }) {
+                Text("ADD")
+            }
+        }
+
+        LazyColumn {
+            items(alltodo) {
+                Row(modifier = Modifier.clickable {
+                    todoviewmodel.changeDone(it)
+                }) {
+                    Text(it.title!!)
+
+                    if(it.done == true) {
+                        Text("DONE")
+                    } else {
+                        Text("NOT DONE")
+                    }
+
+                    Button(onClick = {
+                        todoviewmodel.deleteTodo(it)
+                    }) {
+                        Text("DELETE")
+                    }
+                }
+            }
+        }
 
 
     }
--- app/src/main/java/se/magictechnology/pia13android26feb/TodoViewModel.kt
+++ app/src/main/java/se/magictechnology/pia13android26feb/TodoViewModel.kt
@@ -1,26 +1,100 @@
 package se.magictechnology.pia13android26feb
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.database.getValue
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 
-data class Todo(val title : String? = null, val done : Boolean? = null)
+data class Todo(var fbid : String? = null, val title : String? = null, val done : Boolean? = null)
 
 
 class TodoViewModel : ViewModel() {
 
     val database = Firebase.database.reference
 
+    private val _alltodo = MutableStateFlow(listOf<Todo>())
+    val alltodo: StateFlow<List<Todo>> = _alltodo.asStateFlow()
 
     fun addTodo(todotitle : String) {
-        val newtodo = Todo(todotitle, false)
+        val newtodo = Todo(title = todotitle, done =  false)
 
         database.child("androidtodo").push().setValue(newtodo)
+
+        loadTodo()
     }
 
     fun loadTodo() {
+        database.child("androidtodo").get().addOnSuccessListener { todosnapshot ->
 
+            var loadedtodo = mutableListOf<Todo>()
+            todosnapshot.children.forEach { childsnapshot ->
+                /*
+                val todo = childsnapshot.getValue<Todo>()
+                if(todo != null) {
+                    todo.fbid = childsnapshot.key
+                    loadedtodo.add(todo)
+                }
+                */
+
+                childsnapshot.getValue<Todo>()?.let { todo ->
+                    todo.fbid = childsnapshot.key
+                    loadedtodo.add(todo)
+                }
+
+            }
+
+            _alltodo.value = loadedtodo
+
+        }
     }
+
+    fun changeDone(changetodo : Todo) {
+        var fbid = ""
+        changetodo.fbid?.let {
+            fbid = it
+        }
+        if(fbid == "") {
+            // FAIL
+            return
+        }
+
+        var done = false
+        changetodo.done?.let {
+            done = it
+        }
+        done = !done
+
+        database
+            .child("androidtodo")
+            .child(fbid)
+            .child("done")
+            .setValue(done)
+
+        loadTodo()
+    }
+
+    fun deleteTodo(deltodo : Todo) {
+        var fbid = ""
+        deltodo.fbid?.let {
+            fbid = it
+        }
+        if(fbid == "") {
+            // FAIL
+            return
+        }
+
+        database
+            .child("androidtodo")
+            .child(fbid)
+            .removeValue()
+
+        loadTodo()
+    }
+
 
 }
